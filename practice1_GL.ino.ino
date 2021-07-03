370889c04d3d9ddc0d5e4aa325645293e45cf624
ira-zaya <67481134+ira-zaya@users.noreply.github.com>
2021-07-03 20:59:25 +0300

    Add files via upload

---
+++ practice1_GL.ino.ino
@@ -0,0 +1,79 @@
+#include <LiquidCrystal.h>
+#include <AccelStepper.h>
+#include <GyverTimers.h>
+
+#define motorPin1  8
+#define motorPin2  9
+#define motorPin3  10
+#define motorPin4  11
+
+int val = 0;
+float motorSpeed = 0;
+byte left[8] = {
+  B00000,
+  B00010,
+  B00100,
+  B01000,
+  B11111,
+  B01000,
+  B00100,
+  B00010
+};
+
+byte right[8] = {
+  B00000,
+  B01000,
+  B00100,
+  B00010,
+  B11111,
+  B00010,
+  B00100,
+  B01000
+};
+LiquidCrystal lcd(13, 12, 5, 4, 3, 2);
+AccelStepper stepper_motor(8, motorPin1, motorPin3, motorPin2, motorPin4);
+
+void setup()
+{
+  lcd.createChar(0, left);
+  lcd.createChar(1, right);
+  lcd.begin(16, 2);//объявляем "рабочее пространство" lcd: 16 сегментов, 2 строки
+  Timer1.setFrequency(5); // частота в Герцах
+  Timer1.enableISR(); // вкл прерывания
+  Timer2.setFrequency(5); // частота в Герцах
+  Timer2.enableISR(); // вкл прерывания
+}
+
+void loop()
+{
+  stepper_motor.runSpeed();
+}
+
+ISR(TIMER1_A) {  // обработчик прерывания
+  lcd.clear();
+  lcd.print("ADC:");
+  lcd.setCursor(0, 1);
+  lcd.print("Speed:");
+  lcd.setCursor(7, 1);
+  lcd.print(abs (motorSpeed * 4 / 1000 * 4));
+  val = analogRead(A0);
+  lcd.setCursor(5, 0);
+  lcd.print(val);
+  lcd.setCursor(11, 1);
+  lcd.print("r/m");
+  lcd.setCursor(12, 0);
+  lcd.print(millis()/1000);
+  r_direction();
+}
+
+ISR(TIMER2_A) {
+  stepper_motor.setMaxSpeed(1000);// может делать ~60000 шагов в минуту (~15 оборотов в минуту)
+  val = analogRead(A0);
+  motorSpeed = map(val, 0, 1023, -1000, 1000);
+  stepper_motor.setSpeed(motorSpeed);
+}
+
+void r_direction() {
+  lcd.setCursor(15, 1);
+  (motorSpeed < 0) ? lcd.write((uint8_t)1) : lcd.write((uint8_t)0);
+}
